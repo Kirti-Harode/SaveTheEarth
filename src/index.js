a28310bb103435d6ca3aed5281d3cdf6364f4e60
Kirti Harode <kikihrd20@gmail.com>
2022-01-02 22:51:07 -0800

    I can see and murcury

--- src/index.js
+++ src/index.js
@@ -1,16 +1,36 @@
-
+import {Planet} from './scripts/planets';
 window.addEventListener('DOMContentLoaded', (event) => {
     const canvas = document.getElementById("mycanvas");
     const ctx = canvas.getContext("2d");
 
     ctx.width = window.innerWidth;
     ctx.height = window.innerHeight;
-    console.log("everything is fine, you can do this");
-    ctx.fillStyle = 'rgba(0,0,0,0.4)';
-  ctx.strokeStyle = 'rgba(0,153,255,0.4)';
-  ctx.save();
-    
-});
 
+    ctx.fillStyle = "rgb(1, 10, 10)";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    console.log("everything is fine, you can do this");
+
+
+    // let planets = [];
+
+    // function init(){
+    //     planets.push(new Planet(canvas.width/2,canvas.height/2, 35, 0, 0, 'yellow')); // sun
+    //     planets.push(new Planet(canvas.width/2,canvas.height/2, 5, 5, 65, 'gray')); // mercury
+    //     planets.push(new Planet(canvas.width/2,canvas.height/2, 10, 4, 90, 'orange')); // venus
+    //     planets.push(new Planet(canvas.width/2,canvas.height/2, 15, 3, 125, 'blue')); // earth
+    //     planets.push(new Planet(canvas.width/2,canvas.height/2, 20, 3.5, 175, 'red')); // mars
+    // }
+
+    let sun = new Planet(canvas.width/2, canvas.height/2, 35, 'yellow',0, 0); 
+    sun.draw();
+
+    let murcury = new Planet(canvas.width/2,canvas.height/2, 5, 'gray', 5, 65);
+    murcury.draw();
+    // murcury.animate();
+    murcury.update();
+    
+
+
+});
 
 
--- src/scripts/planets.js
+++ src/scripts/planets.js
@@ -3,18 +3,60 @@ const canvas = document.getElementById("mycanvas");
 const ctx = canvas.getContext("2d");
 
 ctx.moveTo(250, 50)
-class Planet{
-    constructor(x, y, radius, vel, orbitRadius){
+export class Planet{
+    constructor(x, y, radius, color, vel, orbitRadius){
         this.x = x;
         this.y = y;
         this.radius = radius;
+        this.color = color;
         this.vel = vel;
         this.orbitRadius = orbitRadius;
+        this.startX = x;
+        this.startY = y;
     }
 
     draw(){
-     
-        
 
+        // ctx.beginPath();
+        // ctx.lineWidth = 2;
+        // ctx.arc(
+        //     this.startX,
+        //     this.startY,
+        //     this.orbitRadius,
+        //     0,
+        //     Math.PI * 2,
+        //     false
+        // );
+        // ctx.strokeStyle = 'rgba(255, 255, 255, 0.35)';
+        // ctx.stroke();
+
+
+        ctx.shadowBlur = 15;
+        ctx.shadowClor = this.color;
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, (Math.PI * 2), false);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+        ctx.shadowBlur = 0
     }
+
+    update(){
+        this.draw();
+        if(this.vel > 0){
+            this.x = this.startX + Math.cos(this.radian) * this.orbitRadius;
+            this.y = this.startY + Math.sin(this.radian) * this.orbitRadius;
+        }
+    }
+
+    animate(){
+        window.requestAnimationFrame(this.animate);
+        ctx.clearRect(0, 0, canvas.width, canvas.hight);
+        // ctx.fillStyle = 'rgb(0, 0, 0)';
+        ctx.fillStyle = this.color;
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+        this.update();
+    }
+    
+
 }
